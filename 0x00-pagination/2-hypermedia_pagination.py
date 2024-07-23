2254210b40eedcb73b0a58051e02e0f89a3bfe22
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-07-23 05:10:39 +0300

    Fix: Typo error

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -65,5 +65,5 @@ class Server:
             "data": data,
             "next_page": next_page,
             "prev_page": prev_page,
-            "tota_pages": total_pages
+            "total_pages": total_pages
         }
